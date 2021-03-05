67a39b0ab87e053166cf879412d62ac8e3b1201a
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2021-03-05 16:13:19 +0000

    Fix failure of non-rose workflows Cause: Cylc rose trying to dump config where config == False. Fix: Prevent cylc-rose dumping config if config not present.

--- tests/test_functional_post_install.py
+++ tests/test_functional_post_install.py
@@ -277,3 +277,13 @@ def test_rose_fileinstall_exception(tmp_path, monkeypatch):
     (tmp_path / 'rose-suite.conf').touch()
     with pytest.raises(FileNotFoundError):
         rose_fileinstall(srcdir=tmp_path, rundir=tmp_path)
+
+
+def test_cylc_no_rose(tmp_path):
+    """A Cylc workflow that contains no ``rose-suite.conf`` installs OK.
+    """
+    from cylc.rose.entry_points import post_install
+    assert post_install(srcdir=tmp_path, rundir=tmp_path) == {
+        'record_install': False,
+        'fileinstall': False
+    }
