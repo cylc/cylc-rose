5a9b01c5c76a3028f8e09503bcfdb1ec747dcca6
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-08-09 14:55:17 +0100

    put it back

--- tests/functional/test_rose_stem.py
+++ tests/functional/test_rose_stem.py
@@ -244,6 +244,7 @@ def rose_stem_run_template(setup_stem_repo, pytestconfig):
             **setup_stem_repo
         }
     yield _inner_fn
+    shutil.rmtree(setup_stem_repo['suite_install_dir'])
 
 
 @pytest.fixture(scope='class')
