ba08059fb78baa92706821d9906df9265465a42a
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-08-12 15:35:05 +0100

    fixed checking for failure before cleanup

--- tests/functional/test_rose_stem.py
+++ tests/functional/test_rose_stem.py
@@ -173,7 +173,10 @@ def setup_stem_repo(tmp_path_factory, monkeymodule, request):
         'suite_install_dir': suite_install_dir
     }
     # Only clean up if all went well.
-    if not request.session.testsfailed:
+    if all([
+        i.outcome == 'passed' or i.outcome == 'skipped'
+        for i in request.node.obj._module_outcomes.values()
+    ]):
         shutil.rmtree(suite_install_dir)
 
 
