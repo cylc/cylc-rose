39339e77332a1be6574d373c17fd939b5901dcde
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-08-26 13:20:38 +0100

    Ensured that opts.source is not unset when opts is passed to the StemRunner object
    update the help docs to match Rose 2019
    
    fix broken test
    
    increase or decrease the verbosity of the logging.

--- cylc/rose/stem.py
+++ cylc/rose/stem.py
@@ -23,25 +23,53 @@ Install a rose-stem suite using cylc install.
 
 To run a rose-stem suite use "cylc play".
 
-Looks for a suite to install either in $PWD/rose-stem, or in a specified
-location - e.g:
+Install a suitable suite with a specified set of source tree(s).
 
-# Install a rose-stem suite from PWD/rose-stem
-$ rose stem
+Default values of some of these settings are suite-dependent, specified
+in the `rose-suite.conf` file.
 
-# Install a rose-stem suite from PWD/relative-path.
-$ rose stem relative-path
+Examples
 
-# Install a rose-stem suite from specified abs path.
-$ rose stem /absolute/path
+    rose stem --group=developer
+    rose stem --source=/path/to/source --source=/other/source --group=mygroup
+    rose stem --source=foo=/path/to/source --source=bar=fcm:bar_tr@head
+
+Jinja2 Variables
+
+    Note that `<project>` refers to the FCM keyword name of the repository in
+    upper case.
+
+    HOST_SOURCE_<project>
+        The complete list of source trees for a given project. Working copies
+        in this list have their hostname prefixed, e.g. `host:/path/wc`.
+    HOST_SOURCE_<project>_BASE
+        The base of the project specified on the command line. This is
+        intended to specify the location of `fcm-make` config files. Working
+        copies in this list have their hostname prefixed.
+    RUN_NAMES
+        A list of groups to run in the rose-stem suite.
+    SOURCE_<project>
+        The complete list of source trees for a given project. Unlike the
+        `HOST_` variable of similar name, paths to working copies do NOT
+        include the host name.
+    SOURCE_<project>_BASE
+        The base of the project specified on the command line. This is
+        intended to specify the location of `fcm-make` config files. Unlike
+        the `HOST_` variable of similar name, paths to working copies do NOT
+        include the host name.
+    SOURCE_<project>_REV
+        The revision of the project specified on the command line. This
+        is intended to specify the revision of `fcm-make` config files.
 """
 
+from ansimarkup import parse as cparse
 from contextlib import suppress
 import os
 from pathlib import Path
 import re
 import sys
 
+from cylc.flow.exceptions import CylcError
 from cylc.flow.scripts.install import (
     get_option_parser,
     install as cylc_install
@@ -55,6 +83,7 @@ from metomi.rose.reporter import Reporter, Event
 from metomi.rose.resource import ResourceLocator
 
 
+EXC_EXIT = cparse('<red><bold>{name}: </bold>{exc}</red>')
 DEFAULT_TEST_DIR = 'rose-stem'
 ROSE_STEM_VERSION = 1
 SUITE_RC_PREFIX = '[jinja2:suite.rc]'
@@ -96,7 +125,7 @@ class NameSetEvent(Event):
     __str__ = __repr__
 
 
-class ProjectNotFoundException(Exception):
+class ProjectNotFoundException(CylcError):
 
     """Exception class when unable to determine project a source belongs to."""
 
@@ -482,6 +511,7 @@ def get_source_opt_from_args(opts, args):
     """
     if len(args) == 0:
         # sourcedir not given:
+        opts.source = None
         return opts
     elif os.path.isabs(args[-1]):
         # sourcedir given, and is abspath:
@@ -517,22 +547,40 @@ def main():
         dest="stem_sources")
 
     # Hard-set for now, but could be set based upon cylc verbosity levels?
-    parser.add_option('--verbosity', default=1)
-    parser.add_option('--quietness', default=0)
+    parser.add_option(
+        '--verbosity', '-v', default=1,
+        help='Increase the verbosity of logging.'
+    )
+    parser.add_option(
+        '--quietness', '-q', default=0,
+        help='Decrease the verbosity of logging.'
+    )
     parser.usage = __doc__
 
     opts, args = parser.parse_args(sys.argv[1:])
     # sliced sys.argv to drop 'rose-stem'
     opts = get_source_opt_from_args(opts, args)
 
-    # modify the CLI options to add whatever rose stem would like to add
-    opts = StemRunner(opts).process()
+    try:
+        # modify the CLI options to add whatever rose stem would like to add
+        opts = StemRunner(opts).process()
 
-    # call cylc install
-    if hasattr(opts, 'source'):
-        cylc_install(parser, opts, opts.source)
-    else:
-        cylc_install(parser, opts)
+        # call cylc install
+        if hasattr(opts, 'source'):
+            cylc_install(parser, opts, opts.source)
+        else:
+            cylc_install(parser, opts)
+
+    except CylcError as exc:
+        if opts.verbosity > 1:
+            raise exc
+        print(
+            EXC_EXIT.format(
+                name=exc.__class__.__name__,
+                exc=exc
+            ),
+            file=sys.stderr
+        )
 
 
 if __name__ == "__main__":
