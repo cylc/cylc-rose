072e08c255c295b66632df166dc5fd8347e255f6
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-10-04 09:09:02 +0100

    test lowercase booleans in template vars

--- tests/unit/test_rose_stem_units.py
+++ tests/unit/test_rose_stem_units.py
@@ -17,15 +17,9 @@
 """
 
 import pytest
-from pytest import param
 from types import SimpleNamespace
 
-from cylc.rose.stem import (
-    get_source_opt_from_args, StemRunner, SUITE_RC_PREFIX)
-
-from metomi.rose.reporter import Reporter
-from metomi.rose.popen import RosePopener
-from metomi.rose.fs_util import FileSystemUtil
+from cylc.rose.stem import get_source_opt_from_args
 
 
 @pytest.mark.parametrize(
@@ -60,53 +54,3 @@ def test_get_source_opt_from_args(tmp_path, monkeypatch, args, expect):
         assert result == expect
     else:
         assert result == expect.format(tmp_path=str(tmp_path))
-
-
-@pytest.fixture
-def get_StemRunner():
-    def _inner(kwargs, options=None):
-        if options is None:
-            options = {}
-        """Create a StemRunner objects with some options set."""
-        opts = SimpleNamespace(**{'verbosity': 1, 'quietness': 1})
-        for k, v in options.items():
-            setattr(opts, k, v)
-        stemrunner = StemRunner(opts, **kwargs)
-        return stemrunner
-    return _inner
-
-
-@pytest.mark.parametrize(
-    'attribute, object_, kwargs', [
-        param('reporter', Reporter, {}, id='reporter default'),
-        param('reporter', str, {'reporter': 'foo'}, id='reporter set'),
-        param('popen', RosePopener, {}, id='popen default'),
-        param('popen', str, {'popen': 'foo'}, id='popen set'),
-        param('fs_util', FileSystemUtil, {}, id='fs_util default'),
-        param('fs_util', str, {'fs_util': 'foo'}, id='fs_util set')
-    ]
-)
-def test_StemRunner_init(get_StemRunner, attribute, object_, kwargs):
-    """It handles __init__ with different kwargs."""
-    stemrunner = get_StemRunner(kwargs)
-    item = getattr(stemrunner, attribute)
-    if isinstance(object_, str):
-        assert item == kwargs[attribute]
-    else:
-        assert isinstance(item, object_)
-
-
-@pytest.mark.parametrize(
-    'exisiting_defines',
-    [
-        param([], id='no existing defines'),
-        param(['opts=(cylc-install)'], id='existing defines')
-    ]
-)
-def test__add_define_option(get_StemRunner, capsys, exisiting_defines):
-    """It adds to defines, rather than replacing any."""
-    stemrunner = get_StemRunner(
-        {'reporter': print}, {'defines': exisiting_defines})
-    assert stemrunner._add_define_option('FOO', '"bar"') is None
-    assert f'{SUITE_RC_PREFIX}FOO="bar"' in stemrunner.opts.defines
-    assert 'Variable FOO set to "bar"' in capsys.readouterr().out
