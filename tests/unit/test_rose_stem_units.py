9974e2b51cddb9244d80e053832614bae6d2f225
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-10-21 10:24:10 +0100

    feature: stem installs to workflow name not `rose-stem` (#180)
    * make rose stem install to a sensible name workflow

--- tests/unit/test_rose_stem_units.py
+++ tests/unit/test_rose_stem_units.py
@@ -16,10 +16,11 @@
 """Functional tests for top-level function record_cylc_install_options and
 """
 
+import os
 import pytest
 from types import SimpleNamespace
 
-from cylc.rose.stem import get_source_opt_from_args
+from cylc.rose.stem import get_source_opt_from_args, StemRunner
 
 
 @pytest.mark.parametrize(
@@ -48,9 +49,63 @@ def test_get_source_opt_from_args(tmp_path, monkeypatch, args, expect):
     monkeypatch.chdir(tmp_path)
     opts = SimpleNamespace()
 
-    result = get_source_opt_from_args(opts, args).source
+    result = get_source_opt_from_args(opts, args).workflow_conf_dir
 
     if expect is None:
         assert result == expect
     else:
         assert result == expect.format(tmp_path=str(tmp_path))
+
+
+class Test_generate_name:
+    """Try 3 paths in StemRunner._generate_name.
+
+    Contains a setup function for each path.
+    """
+    EXPECT = 'bar'
+    MSG = 'Suite is named bar'
+
+    @staticmethod
+    def name_from_fcm(monkeypatch):
+        """It gets the name from _ascertain_project.
+        """
+        class MonkeyStemRunner(StemRunner):
+            def _ascertain_project(self, x):
+                return ['foo', '/foo/bar']
+        return MonkeyStemRunner(SimpleNamespace, reporter=print)
+
+    @staticmethod
+    def source_set(monkeypatch):
+        """It gets the name from the source opt.
+
+        equivalent of using:
+            rose stem /foo/bar/rose-stem
+        """
+        opts = SimpleNamespace()
+        opts.workflow_conf_dir = '/foo/bar'
+        stemrunner = StemRunner(opts, reporter=print)
+        return stemrunner
+
+    @staticmethod
+    def source_unset(monkeypatch):
+        """It gets the name from the file path.
+
+        equivalent of using:
+            rose stem
+        """
+        opts = SimpleNamespace()
+        opts.workflow_conf_dir = ''
+        monkeypatch.setattr(os, "getcwd", lambda: "/foo/bar/rose-stem")
+        stemrunner = StemRunner(opts, reporter=print)
+        return stemrunner
+
+    @pytest.mark.parametrize(
+        'setup', [
+            'source_set', 'source_unset', 'name_from_fcm'])
+    def test__generate_name(self, capsys, monkeypatch, setup):
+        """Tests StemRunner._generate_name for fake stemrunner
+        returned by each test case method.
+        """
+        result = getattr(self, setup)(monkeypatch)._generate_name()
+        assert result == self.EXPECT
+        assert self.MSG in capsys.readouterr().out
